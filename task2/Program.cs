eb01a025446f5a085b3547c3745a77523000338a
Anthelena <proffy_2010@mail.ru>
2022-12-16 17:19:50 +0300

    Задача 50

---
+++ task2/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает элемент в двумерном массиве, и возвращает индекс этого элемента или же указание, что такого элемента нет. 
+// Если элементов несколько, то выводим позицию каждого. Двумерный массив задаётся на ваш выбор.
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+
+Console.Write("Введите строку: ");
+int pos1 = Convert.ToInt32(Console.ReadLine()) - 1;
+Console.Write("Введите столбец: ");
+int pos2 = Convert.ToInt32(Console.ReadLine()) - 1;
+int n = 4; 
+int m = 5; 
+Random random = new Random();
+int[,] array = new int[n, m];
+Console.WriteLine("Исходный массив: ");
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+{
+    array[i, j] = random.Next(0, 10);
+Console.Write("{0} ", array[i, j]);
+}
+Console.WriteLine();
+}
+    if (pos1 < 0 | pos1 > array.GetLength(0) - 1 | pos2 < 0 | pos2 > array.GetLength(1) - 1)
+{
+Console.WriteLine("Элемент не существует  ");
+}
+    else
+{
+    Console.WriteLine("Значение элемента массива = {0}", array[pos1, pos2]);
+}
